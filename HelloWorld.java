38d1d4524eb6c0f923d3aff482d8c4285245944b
swati0205 <swati0205.in@gmail.com>
2020-02-23 18:33:53 -0500

    HelloWorld class and java added

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+package HelloW;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		for(int i = 1; i<12; i++)
+		{
+			System.out.println("Helllo World.." +i);
+		}
+
+	}
+
+}
